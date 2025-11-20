70a4cd0733ce7dc7afe609f2a618dcfeb5d43284
plain-noodle-expert <yixuan.tang@mail.mcgill.ca>
2025-11-20 14:32:39 -0500

    update .gitignore

--- Debug/Drivers/Components/lps22hb/lps22hb.d
+++
@@ -1,8 +0,0 @@
-Drivers/Components/lps22hb/lps22hb.o: \
- ../Drivers/Components/lps22hb/lps22hb.c \
- ../Drivers/Components/lps22hb/lps22hb.h \
- ../Drivers/Components/lps22hb/../Common/psensor.h \
- ../Drivers/Components/lps22hb/../Common/tsensor.h
-../Drivers/Components/lps22hb/lps22hb.h:
-../Drivers/Components/lps22hb/../Common/psensor.h:
-../Drivers/Components/lps22hb/../Common/tsensor.h:
